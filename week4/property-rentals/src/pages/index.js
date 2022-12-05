cdad7feb407bebd0b7f7c3e5a07c46a23713de96
N Zafar <n_zafar_007@hotmail.com>
2022-12-05 14:48:26 +0100

    refactor

--- week4/property-rentals/src/pages/index.js
+++ week4/property-rentals/src/pages/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import background from '../assets/background.mp4'
+import background from '../assets/background.mp4';
 import Categories from '../components/Categories';
 
-
-
 const Home = () => {
-return (
-	<div className='main'>
-		<video src={background} autoPlay loop muted />
-		<div className='content'>
-			<h1 className='main-title'>Your Dream Vacation</h1>
-			<h2 className='main-subtitle'>5000+ villas for rent worldwide</h2>
-			<Categories />
+	return (
+		<div className='main'>
+			<video src={background} autoPlay loop muted />
+			<div className='content'>
+				<h1 className='main-title'>Your Dream Vacation</h1>
+				<h2 className='main-subtitle'>5000+ villas for rent worldwide</h2>
+				<Categories />
+			</div>
 		</div>
-	</div>
-);
+	);
 };
 
 export default Home;
